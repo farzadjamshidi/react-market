75fda2ab0367b9ec9db25d5b6a6b71dcb1184be2
farzadjamshidi <farzadjamshidicom@yahoo.com>
2021-08-06 15:24:39 +0430

    add axios and connet to backend

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,10 +1,24 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import { Row, Col } from 'react-bootstrap';
 import Product from '../components/Product/Product';
-import products from '../products';
+import axios from 'axios';
 
 const Home = () =>
 {
+    const [products, setProducts] = useState([]);
+
+    useEffect(() =>
+    {
+        const sendRequest = async () =>
+        {
+            const response = await axios.get('http://localhost:8000/api/products');
+
+            setProducts(response.data);
+        };
+
+        sendRequest();
+    }, []);
+
     return <div>
         <h1>محصولات</h1>
         <Row>
--- src/pages/Product.js
+++ src/pages/Product.js
@@ -1,15 +1,25 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import { Link } from 'react-router-dom';
 import { Row, Col, Image, ListGroup, Button } from 'react-bootstrap';
 
-import products from '../products';
+import axios from 'axios';
 
 const Product = ({ match }) =>
 {
-    const product = products.find((item) =>
+
+    const [product, setProduct] = useState([]);
+
+    useEffect(() =>
     {
-        return item._id === match.params.id;
-    });
+        const sendRequest = async () =>
+        {
+            const response = await axios.get('http://localhost:8000/api/products/' + match.params.id);
+
+            setProduct(response.data);
+        };
+
+        sendRequest();
+    }, [match]);
 
     return <div>
         <Link to="/" className="btn btn-light my-3">بازگشت به صفحه اصلی</Link>
